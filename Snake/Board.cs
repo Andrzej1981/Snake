db89dfbde97cf4e79b124b4f5bf5666abed2805e
Andrzej S <AndrzejS21@interia.pl>
2023-06-08 14:35:54 +0200

    Add project files.

---
+++ Snake/Board.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Board : Form
+    {
+        Random rand;
+
+        enum GameBoardFields
+        {
+            Free,Snake,Bonus
+        };
+
+        enum Directions
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        };
+
+        struct SnakeCoordinates
+        {
+            public int x;
+            public int y;
+        }
+
+        GameBoardFields[,] gameBoardFields;
+        SnakeCoordinates[] snakeXY;
+        int snakeLenght;
+
+
+        public Board()
+        {
+            InitializeComponent();
+        }
+
+        private void Board_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            double framerate = 1000 / 5.0;
+            //game loop
+
+            while (!exit)
+            {
+               if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo input = Console.ReadKey();
+
+                    switch(input.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            exit = true;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            break;
+                        case ConsoleKey.RightArrow:
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            break;
+
+                    }
+
+                    if(DateTime.Now)
+                
+
+            }
+
+
+
+        }
+    }
+}
